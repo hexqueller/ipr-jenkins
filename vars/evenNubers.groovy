aa5f5945ac92109900d7ce5bf1e77e74b6d4837c
V1RUSnya <dima.nesed@gmail.com>
2024-07-04 03:02:31 +0300

    another script

---
+++ vars/evenNubers.groovy
@@ -0,0 +1,10 @@
+def call(int max) {
+    for (int i = 0; i <= max; i++) {
+        if (i % 2 == 0) {
+            echo "Even number: ${i}"
+        } else {
+            // Skip
+        }
+    }
+}
+
