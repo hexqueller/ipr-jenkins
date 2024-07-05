60300f715c8aac6ebb108598f9976773163238b5
V1RUSnya <dima.nesed@gmail.com>
2024-07-05 16:29:53 +0300

    Фиксация Vault

--- Makefile
+++ Makefile
@@ -8,3 +8,6 @@ run:
 
 restart: clean
 	docker-compose down && docker-compose up --build
+
+secretsPrune:
+	rm -rf vault/init.file && rm -rf vault/jenkins_approle.file && rm -rf vault/data
--- vault/init.sh
+++ vault/init.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
 export VAULT_ADDR='http://127.0.0.1:8200'
+UNSEAL_KEY_FILE="/vault/unseal.key"
+INIT_FILE="/vault/init.file"
 
-# Проверяем, инициализирован ли Vault
-vault operator init -status
-if [ $? -eq 0 ]; then
+# Проверяем наличие файла init.file
+if [ -f $INIT_FILE ]; then
   echo "Vault is already initialized"
 else
-  # Инициализируем Vault и захватываем root token и ключи
-  vault operator init -key-shares=1 -key-threshold=1 > /vault/init.file
+  # Проверяем, инициализирован ли Vault
+  vault operator init -status
+  if [ $? -eq 0 ]; then
+    echo "Vault is already initialized"
+  else
+    # Инициализируем Vault и захватываем root token и ключи
+    vault operator init -key-shares=1 -key-threshold=1 > $INIT_FILE
+    
+    # Извлекаем и сохраняем ключ распечатки
+    grep 'Unseal Key 1:' $INIT_FILE | awk '{print $NF}' > $UNSEAL_KEY_FILE
+  fi
 fi
 
-VAULT_UNSEAL_KEY=$(grep 'Unseal Key 1:' /vault/init.file | awk '{print $NF}')
-VAULT_ROOT_TOKEN=$(grep 'Initial Root Token:' /vault/init.file | awk '{print $NF}')
+# Проверяем наличие файла unseal.key и используем его для распечатки Vault
+if [ -f $UNSEAL_KEY_FILE ]; then
+  VAULT_UNSEAL_KEY=$(cat $UNSEAL_KEY_FILE)
+  vault operator unseal $VAULT_UNSEAL_KEY
+else
+  echo "Unseal key file not found!"
+  exit 1
+fi
 
-# Распечатываем Vault
-vault operator unseal $VAULT_UNSEAL_KEY
+# Извлекаем root token для входа
+VAULT_ROOT_TOKEN=$(grep 'Initial Root Token:' $INIT_FILE | awk '{print $NF}')
 
 # Логинимся с root token
 vault login $VAULT_ROOT_TOKEN
 
+# Создаем AppRole для Jenkins
+vault auth enable approle
+vault write auth/approle/role/jenkins-role token_num_uses=0 secret_id_num_uses=0 policies="jenkins"
+
+# Получаем и сохраняем Role ID и Secret ID в файл
+echo "ROLE_ID: $(vault read auth/approle/role/jenkins-role/role-id -format=json)" > /vault/jenkins_approle.file
+echo "SECRET_ID: $(vault write -f auth/approle/role/jenkins-role/secret-id -format=json)" >> /vault/jenkins_approle.file
+
 # Создаем путь для секретов и добавляем секреты
-vault secrets enable -path=jenkins kv
-vault kv put jenkins/api-token value=my-secret-api-token
+vault secrets enable -path=secrets kv
+vault write secrets/creds/jenkins username=jenkins password=jenkins
+vault write secrets/creds/api-key secret=SuperDuperSecretApiKey
+
+# Загружаем политику
+vault policy write jenkins vault/jenkins-policy.hcl
