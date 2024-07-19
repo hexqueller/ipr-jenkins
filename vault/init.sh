b0523c49dade2567bb8c6e6b99a354cdb204271e
V1RUSnya <dima.nesed@gmail.com>
2024-07-19 11:08:07 +0300

    try to update creds on rest

--- vault/init.sh
+++ vault/init.sh
@@ -40,17 +40,11 @@ vault login $VAULT_ROOT_TOKEN
 vault auth enable approle
 vault write auth/approle/role/jenkins-role token_num_uses=0 secret_id_num_uses=0 policies="jenkins"
 
-# Получаем и сохраняем Role ID и Secret ID в файл
-if [ -f /secrets.file ]; then
-  echo "Secrets.file is already initialized"
-else
-  ROLE_ID=$(vault read auth/approle/role/jenkins-role/role-id -format=json | grep '"role_id"' | sed -E 's/.*"role_id": "(.*)".*/\1/')
-  SECRET_ID=$(vault write -f auth/approle/role/jenkins-role/secret-id -format=json | grep '"secret_id"' | sed -E 's/.*"secret_id": "(.*)".*/\1/')
-
-  echo "ROLE_ID: ${ROLE_ID}" > /secrets.file
-  echo "SECRET_ID: ${SECRET_ID}" >> /secrets.file
-  echo "ROOT_TOKEN: $VAULT_ROOT_TOKEN" >> /secrets.file
-fi
+ROLE_ID=$(vault read auth/approle/role/jenkins-role/role-id -format=json | grep '"role_id"' | sed -E 's/.*"role_id": "(.*)".*/\1/')
+SECRET_ID=$(vault write -f auth/approle/role/jenkins-role/secret-id -format=json | grep '"secret_id"' | sed -E 's/.*"secret_id": "(.*)".*/\1/')
+echo "ROLE_ID: ${ROLE_ID}" > /secrets.file
+echo "SECRET_ID: ${SECRET_ID}" >> /secrets.file
+echo "ROOT_TOKEN: $VAULT_ROOT_TOKEN" >> /secrets.file
 
 # Создаем путь для секретов и добавляем секреты
 vault secrets enable -path=secrets kv
@@ -59,3 +53,42 @@ vault write secrets/creds/api-key secret=SuperDuperSecretApiKey
 
 # Загружаем политику
 vault policy write jenkins vault/jenkins-policy.hcl
+
+
+# Отправляем на Jenkins обновленные данные!
+VAULT_CREDENTIALS=$(cat <<EOF
+{
+  "scope": "GLOBAL",
+  "usePolicies": false,
+  "vaultAppRoleCredential": {
+    "description": "vault-jenkins-role",
+    "id": "vault-jenkins-role",
+    "path": "approle",
+    "roleId": "$ROLE_ID",
+    "secretId": "$SECRET_ID"
+  }
+}
+EOF
+)
+
+JENKINS_URL="http://jenkins:8080"
+JENKINS_USER="jenkins-admin"
+JENKINS_PASSWORD="admin"
+
+# Получаем CSRF-токен и поле токена
+CRUMB_RESPONSE=$(curl -s -u "$JENKINS_USER:$JENKINS_PASSWORD" "$JENKINS_URL/crumbIssuer/api/json")
+CRUMB=$(echo $CRUMB_RESPONSE | jq -r '.crumb')
+CRUMB_FIELD=$(echo $CRUMB_RESPONSE | jq -r '.crumbRequestField')
+
+# Проверка значений
+echo "CRUMB: $CRUMB"
+echo "CRUMB_FIELD: $CRUMB_FIELD"
+
+# Получаем ID существующей записи
+VAULT_CREDENTIAL_ID=$(curl -s -u "$JENKINS_USER:$JENKINS_PASSWORD" "$JENKINS_URL/credentials/store/system/domain/_/credential/vaultAppRoleCredential/config.xml" | grep '<id>' | sed -E 's/.*<id>(.*)<\/id>.*/\1/')
+
+# Проверка значения ID
+echo "VAULT_CREDENTIAL_ID: $VAULT_CREDENTIAL_ID"
+
+# Обновляем vaultAppRoleCredential
+curl -X POST -u "$JENKINS_USER:$JENKINS_PASSWORD" -H "$CRUMB_FIELD:$CRUMB" --data-urlencode "json=$VAULT_CREDENTIALS" "$JENKINS_URL/credentials/store/system/domain/_/credential/$VAULT_CREDENTIAL_ID/config.xml"
